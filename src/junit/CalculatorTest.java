3a102f069c89fd92cbf9358ff1560878adb6ab00
UnremittingFighting <1448554314@qq.com>
2019-08-28 17:41:36 +0800

    SX1816090 第二次提交

---
+++ src/junit/CalculatorTest.java
@@ -0,0 +1,22 @@
+package junit;
+
+public class CalculatorTest {
+
+	public int add(int a, int b) {
+		return a + b;
+
+	}
+
+	public int substract(int a, int b) {
+		return a - b;
+
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
---
+++ src/junit/CalculatorTestTest.java
@@ -0,0 +1,45 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTestTest {
+	@BeforeClass
+	public static void setupBeforeClass() throws Exception {
+
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubstract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
